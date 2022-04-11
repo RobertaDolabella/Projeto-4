f19761c81baeda7c7240520ae281ae5a6767e32f
RobertaDolabella <dolabella.roberta@gmail.com>
2022-04-11 10:57:51 -0300

    Problema ao virar a carata. Rever function gerarCarta e mostrarCartas

--- java.js
+++ java.js
@@ -1,6 +1,8 @@
 let quantasCartas;
-//PEGANDO AS IMAGENS DO HTML
-
+let listadeFiguras = []
+let listaDuplicada=[]
+let inputdeCartasBaixo;
+let inputdeCartasCima;
 function comecar(){
     quantasCartas = prompt("Com quantas cartas deseja jogar?")
     if(quantasCartas%2 !== 0 || quantasCartas%2 == isNaN ){
@@ -9,7 +11,7 @@ function comecar(){
         while(quantasCartas%2 !== 0 ||quantasCartas%2 == isNaN){
         alert("O numero de cartas deve ser par")
         quantasCartas = prompt("Com quantas cartas deseja jogar?")
-    }
+        }
     }
 }
 comecar()
@@ -18,7 +20,7 @@ function colocarCartas(){
     let contador=0 
     while(contador<quantasCartas/2){
         inputdeCartasCima.innerHTML +=`
-            <div class="carta">
+            <div class="carta" onclick="mostrarCartas(this,${contador})">
                 <img src="/imagens/front.png" alt="" width="100px" height="100px" >
             </div>`
     contador++
@@ -26,14 +28,12 @@ function colocarCartas(){
     const inputdeCartasBaixo = document.querySelector(".baixo") 
     while(contador<quantasCartas){
         inputdeCartasBaixo.innerHTML +=`
-            <div class="carta">
-                <img src="/imagens/front.png" alt="" width="100px" height="100px" class="posicao${contador}" onclick="viraraCarta(this)">
+            <div class="carta" onclick="mostrarCartas(this,${contador})">
+                <img src="/imagens/front.png" alt="" width="100px" height="100px">
             </div>`
     contador++
     }
 }
-colocarCartas()
-
 function comparador() { 
 	return Math.random() - 0.5; 
 
@@ -41,35 +41,52 @@ function comparador() {
 function gerarCartas(){
     let listaHtml = document.querySelector(".gif").querySelectorAll("img")
     let i =0 
-    let listaDuplicada=[]
     while(i<7){
         listaDuplicada.push(listaHtml[i])
         listaDuplicada.push(listaHtml[i])
         i = i+1
     }
+    console.log(listaDuplicada)
     listaDuplicada.splice(quantasCartas,13)
     let tamanho = listaDuplicada.length
-    let listadeFiguras=[]
     i=0
+    let listadeImagens=[]
     while(i<tamanho){
-        listadeFiguras.push({imagem: listaDuplicada[i], posicao:"indefinida", status:"fechado"})
+        listadeImagens.push({imagem: listaDuplicada[i], posicao:"indefinida", status:"fechado"})
         i++
     }
+    console.log(tamanho)
     let index= []
     i=0
     while(i<tamanho){
         index[i] = i
         i++
     }
-    let posicao = index.sort(comparador);   
+    let listaAleatoria = index.sort(comparador);   
     i=0  
     while(i<tamanho){
-        listadeFiguras[i].posicao= posicao[i]
+        listadeImagens[i].posicao= listaAleatoria[i]
         i++
     }
+    listadeFiguras = listadeImagens
     console.log(listadeFiguras)
 }
-gerarCartas()
-function mostrarCartas(){
+
+
+function mostrarCartas(elemento, contador){
+    let i = 0
+    let fila = listadeFiguras[0].posicao
+    while(fila!=contador){
+        i++
+        fila = listadeFiguras[i].posicao
+    }
+    console.log(fila)
+    console.log(listadeFiguras[i].imagem)
+    elemento.innerHTML = `
+    <div class="carta">
+        ${listadeFiguras[i].imagem}
+    </div>`
 
 }
+colocarCartas()
+gerarCartas()
